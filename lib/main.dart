4589c2a7a70b037c8276334dbcc575da2fa58abb
FlutLab.io <>
2021-03-17 12:28:21 +0000

    Autosave

--- lib/main.dart
+++ lib/main.dart
@@ -3,6 +3,7 @@ import 'package:flutter/material.dart';
 void main() => runApp(
       MaterialApp(
         home: Scaffold(
+            backgroundColor: Colors.blueGrey,
           appBar: AppBar(
             backgroundColor: Colors.blueGrey[500],
             title: Text('I am Poor'),
